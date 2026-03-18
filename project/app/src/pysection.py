42ad482253858feb21b2037cfb677e81775ad39d
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2026-03-18 00:55:11 -0600

    it's working

--- project/app/src/pysection.py
+++ project/app/src/pysection.py
@@ -8,43 +8,50 @@ def f(given):
     return f
 
 
-def bisection(request, a, b):
+def bisection(request):
     if request.method =='POST':
+        if request.session.get('result_str'):
+            request.session.pop('result_str')
+
         givenFunction = request.POST.get('function')
+        pairs = findIntervals(givenFunction)
+
         func = f(givenFunction)
-        fa = func(a)
+
         tolerance = 0.01
+        iteration = 0
+        max_iterations = 100
 
-        while ((b) - (a)) > tolerance:
-            c = ((a) + (b))/2
-            fc = func(c)
+        result_str = ""
 
-            if fc * fa < 0:
-                b = c
-            elif fc * fa > 0:
-                a = c
-                fa = fc
-        result = (a + b)/2
-        request.session['result_str'] = f'{result:6f}'
+        for pair in pairs:
+            
+            a_ = pair[0]
+            b_ = pair[1]
+            fa = func(a_)
+            while ((b_) - (a_)) > tolerance and iteration < max_iterations:
+                c = ((a_) + (b_))/2
+                fc = func(c)
+
+                if fc * fa < 0:
+                    b_ = c
+                elif fc * fa > 0:
+                    a_ = c
+                    fa = fc
+            result = (a_ + b_)/2
+            result_str += f'Raíz en: {result:6f} '
+        request.session['result_str'] = result_str
     return redirect('index')
 
-# For testing only
-def pysection(a, b, givenFunction):
+def findIntervals(givenFunction):
     func = f(givenFunction)
-    fa = func(a)
-    tolerance = 0.01
-
-    while ((b) - (a)) > tolerance:
-        c = ((a) + (b))/2
-        fc = func(c)
-
-        if fc * fa < 0:
-            b = c
-        elif fc * fa > 0:
-            a = c
-            fa = fc
-
-    print((a + b)/2)
-
-if __name__ == "__main__":
-    pysection(0, 1, 'x**4 + 3*x**3 -2')
+    i = -100
+    step = 0.5
+    pairs = []
+    while i < 100:
+        fi = func(i)
+        fj = func(i+step)
+        if (fi*fj) < 0:
+            pairs.append([i, i+step])       
+        i += step
+    return pairs
